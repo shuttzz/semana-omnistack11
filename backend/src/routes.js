12dc66684c73b7226c6a2c64b3c75874144011b9
José Assis de Menezes Neto <netomenezesucg@gmail.com>
2020-03-25 22:23:19 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+// Exportar a variável routes para ser utilizada em outros aquivos
+module.exports = routes;
